5cc0d2b13c3424073af2c87138f68553ecd0f767
Hat <cool97438@gmail.com>
2024-12-05 17:41:42 +0500

    Добавьте файлы проекта.

---
+++ Client_Kurlishuk/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Xml.Serialization;
+
+namespace Client_Kurlishuk
+{
+    internal class Program
+    {
+        static IPAddress ServerIpAddress;
+        static int ServerPort;
+
+        static string ClientToken;
+        static DateTime ClientDateConnection;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void OnSettings()
+        {
+            string Path = Directory.GetCurrentDirectory() + "/.config";
+            if (File.Exists(Path))
+            {
+                StreamReader streamReader = new StreamReader(Path);
+                ServerIpAddress = IPAddress.Parse(streamReader.ReadLine());
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
